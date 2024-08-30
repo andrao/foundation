56ce3567d8610f9201705561ff99baeb7ff38b34
Andrew Lake <awmlake@gmail.com>
2024-08-29 21:56:25 -0300

    feat(db/Entry): rm org_id, user_id

--- packages/@app-server/db/src/schema/tables/Entry.ts
+++ packages/@app-server/db/src/schema/tables/Entry.ts
@@ -11,16 +11,9 @@ export const Entry = createTable({
         entry_id: varchar('entry_id', { length: 36 }).primaryKey(),
 
         project_id: varchar('project_id', { length: 36 }).notNull(),
-
         status: mysqlEnum('status', ['unprocessed', 'processed']).notNull().default('unprocessed'),
-
-        org_id: varchar('org_id', { length: 36 }),
-        user_id: varchar('user_id', { length: 36 }),
     },
     extraConfig: table => ({
-        idx_org_id: index('idx_org_id').on(table.org_id),
-        idx_user_id: index('idx_user_id').on(table.user_id),
-
         // Foreign keys
         idx_project_id: index('idx_project_id').on(table.project_id),
     }),
